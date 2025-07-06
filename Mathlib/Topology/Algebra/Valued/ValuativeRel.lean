63db30cf95acfa40dc1cd5f3ab90753bb44dfcc8
Yakov Pechersky <ffxen158@gmail.com>
2025-07-06 16:31:55 -0400

    ValuativeTopology of NormedField

--- Mathlib/Topology/Algebra/Valued/ValuativeRel.lean
+++ Mathlib/Topology/Algebra/Valued/ValuativeRel.lean
@@ -5,6 +5,7 @@ Authors: Yakov Pechersky
 -/
 import Mathlib.RingTheory.Valuation.ValuativeRel
 import Mathlib.Topology.Algebra.Valued.ValuationTopology
+import Mathlib.Topology.UniformSpace.Ultra.Basic
 
 /-!
 
@@ -17,28 +18,25 @@ to facilitate a refactor.
 
 -/
 
-namespace ValuativeRel
-
-variable {R : Type*} [CommRing R]
-
-instance [UniformSpace R] [IsUniformAddGroup R] [ValuativeRel R] [ValuativeTopology R] :
-    Valued R (ValueGroupWithZero R) :=
-  .mk (valuation R) ValuativeTopology.mem_nhds_iff
-
-end ValuativeRel
-
 namespace ValuativeTopology
 
-variable {R : Type*} [CommRing R] [ValuativeRel R] [TopologicalSpace R] [ValuativeTopology R]
+variable {R : Type*} [CommRing R] [ValuativeRel R] [TopologicalSpace R]
 
-open ValuativeRel TopologicalSpace Filter Topology Set
+open ValuativeRel TopologicalSpace Filter Topology Set Uniformity
 
 local notation "v" => valuation R
 
+lemma of_hasBasis (h : (𝓝 (0 : R)).HasBasis (fun _ ↦ True)
+    fun γ : (ValueGroupWithZero R)ˣ ↦ { x | v x < γ }) :
+    ValuativeTopology R :=
+  ⟨by simp [h.mem_iff]⟩
+
+variable [ValuativeTopology R]
+
 variable (R) in
 theorem hasBasis_nhds_zero :
-    (𝓝 (0 : R)).HasBasis (fun _ => True)
-      fun γ : (ValueGroupWithZero R)ˣ => { x | v x < γ } := by
+    (𝓝 (0 : R)).HasBasis (fun _ ↦ True)
+      fun γ : (ValueGroupWithZero R)ˣ ↦ { x | v x < γ } := by
   simp [Filter.hasBasis_iff, mem_nhds_iff]
 
 variable [IsTopologicalAddGroup R]
@@ -47,6 +45,34 @@ theorem mem_nhds {s : Set R} {x : R} :
     s ∈ 𝓝 x ↔ ∃ γ : (ValueGroupWithZero R)ˣ, { y | v (y - x) < γ } ⊆ s := by
   simp only [← nhds_translation_add_neg x, ← sub_eq_add_neg, preimage_setOf_eq, true_and,
     ((hasBasis_nhds_zero R).comap fun y => y - x).mem_iff]
+
+instance : UniformSpace R := IsTopologicalAddGroup.toUniformSpace R
+
+open Uniformity in
+theorem hasBasis_uniformity : (𝓤 R).HasBasis (fun _ => True)
+    fun γ : (ValueGroupWithZero R)ˣ => { p : R × R | v (p.2 - p.1) < γ } := by
+  rw [uniformity_eq_comap_nhds_zero']
+  exact (hasBasis_nhds_zero R).comap _
+
+instance : IsUniformAddGroup R := isUniformAddGroup_of_addCommGroup
+instance : IsTopologicalRing R := by
+  convert (valuation R).subgroups_basis.toRingFilterBasis.isTopologicalRing
+  rw [TopologicalSpace.ext_iff_nhds]
+  intro x
+  ext s
+  simp [(RingSubgroupsBasis.hasBasis_nhds _ _).mem_iff, mem_nhds, Valuation.ltAddSubgroup]
+
+instance : IsUltraUniformity R := by
+  refine .mk_of_hasBasis hasBasis_uniformity ?_ ?_
+  · intros
+    ext ⟨x, y⟩
+    simp only [preimage_setOf_eq, Prod.snd_swap, Prod.fst_swap, mem_setOf_eq]
+    rw [← Valuation.map_neg, neg_sub]
+  · intro _ _ _ _ _ h h'
+    simp only [mem_setOf_eq] at h h' ⊢
+    have := Valuation.map_add_lt _ h h'
+    ring_nf at this
+    rwa [neg_add_eq_sub] at this
 
 theorem isOpen_ball (r : ValueGroupWithZero R) :
     IsOpen {x | v x < r} := by
@@ -108,6 +134,12 @@ lemma isOpen_sphere {r : ValueGroupWithZero R} (hr : r ≠ 0) :
 end ValuativeTopology
 
 namespace ValuativeRel
+
+variable {R : Type*} [CommRing R]
+
+instance [UniformSpace R] [IsUniformAddGroup R] [ValuativeRel R] [ValuativeTopology R] :
+    Valued R (ValueGroupWithZero R) :=
+  .mk (valuation R) ValuativeTopology.mem_nhds_iff
 
 @[inherit_doc]
 scoped notation "𝒪[" R "]" => Valuation.integer (valuation R)
