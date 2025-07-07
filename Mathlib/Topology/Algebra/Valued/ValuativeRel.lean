be0246120e0671f738f61da5280da9f7aa178132
Yakov Pechersky <ffxen158@gmail.com>
2025-07-06 23:13:14 -0400

    golf using uniform space, and local instance
    local instance prevents tc timeout

--- Mathlib/Topology/Algebra/Valued/ValuativeRel.lean
+++ Mathlib/Topology/Algebra/Valued/ValuativeRel.lean
@@ -18,6 +18,33 @@ to facilitate a refactor.
 
 -/
 
+namespace Valuation
+
+variable {R Γ₀ : Type*} [Ring R] [LinearOrderedCommMonoidWithZero Γ₀]
+  (v : Valuation R Γ₀)
+
+lemma isSymmetricRel_uniformity_ball_lt (r : Γ₀) :
+    IsSymmetricRel { p : R × R | v (p.2 - p.1) < r } := by
+  simp [IsSymmetricRel, Valuation.map_sub_swap]
+
+lemma isSymmetricRel_uniformity_ball_le (r : Γ₀) :
+    IsSymmetricRel { p : R × R | v (p.2 - p.1) ≤ r } := by
+  simp [IsSymmetricRel, Valuation.map_sub_swap]
+
+lemma isTransitiveRel_uniformity_ball_lt (r : Γ₀) :
+    IsTransitiveRel { p : R × R | v (p.2 - p.1) < r } := by
+  intro _ _ _ h h'
+  refine (Valuation.map_add_lt v h h').trans_eq' ?_
+  simp
+
+lemma isTransitiveRel_uniformity_ball_le (r : Γ₀) :
+    IsTransitiveRel { p : R × R | v (p.2 - p.1) ≤ r } := by
+  intro _ _ _ h h'
+  refine (Valuation.map_add_le v h h').trans_eq' ?_
+  simp
+
+end Valuation
+
 namespace ValuativeTopology
 
 variable {R : Type*} [CommRing R] [ValuativeRel R] [TopologicalSpace R]
@@ -46,7 +73,16 @@ theorem mem_nhds {s : Set R} {x : R} :
   simp only [← nhds_translation_add_neg x, ← sub_eq_add_neg, preimage_setOf_eq, true_and,
     ((hasBasis_nhds_zero R).comap fun y => y - x).mem_iff]
 
-instance : UniformSpace R := IsTopologicalAddGroup.toUniformSpace R
+instance : IsTopologicalRing R := by
+  convert (valuation R).subgroups_basis.toRingFilterBasis.isTopologicalRing
+  rw [TopologicalSpace.ext_iff_nhds]
+  intro x
+  ext s
+  simp [(RingSubgroupsBasis.hasBasis_nhds _ _).mem_iff, mem_nhds, Valuation.ltAddSubgroup]
+
+-- this is a local instance to prevent timeouts in typeclass inference
+-- since otherwise, TC search for `UniformSpace R` will start exploring `ValuativeTopology R`
+local instance : UniformSpace R := IsTopologicalAddGroup.toUniformSpace R
 
 open Uniformity in
 theorem hasBasis_uniformity : (𝓤 R).HasBasis (fun _ => True)
@@ -55,13 +91,6 @@ theorem hasBasis_uniformity : (𝓤 R).HasBasis (fun _ => True)
   exact (hasBasis_nhds_zero R).comap _
 
 instance : IsUniformAddGroup R := isUniformAddGroup_of_addCommGroup
-instance : IsTopologicalRing R := by
-  convert (valuation R).subgroups_basis.toRingFilterBasis.isTopologicalRing
-  rw [TopologicalSpace.ext_iff_nhds]
-  intro x
-  ext s
-  simp [(RingSubgroupsBasis.hasBasis_nhds _ _).mem_iff, mem_nhds, Valuation.ltAddSubgroup]
-
 instance : IsUltraUniformity R := by
   refine .mk_of_hasBasis hasBasis_uniformity ?_ ?_
   · intros
@@ -74,16 +103,34 @@ instance : IsUltraUniformity R := by
     ring_nf at this
     rwa [neg_add_eq_sub] at this
 
+lemma uniformity_ball_lt_mem_uniformity {r : ValueGroupWithZero R} (hr : r ≠ 0) :
+    { p : R × R | v (p.2 - p.1) < r } ∈ 𝓤 R := by
+  rw [hasBasis_uniformity.mem_iff]
+  use Units.mk0 r hr
+  simp
+
+lemma uniformity_ball_le_mem_uniformity {r : ValueGroupWithZero R} (hr : r ≠ 0) :
+    { p : R × R | v (p.2 - p.1) ≤ r } ∈ 𝓤 R := by
+  rw [hasBasis_uniformity.mem_iff]
+  rcases le_or_gt 1 r with hr1|hr1
+  · use 1
+    simp only [Units.val_one, setOf_subset_setOf, Prod.forall, true_and]
+    intro _ _ h
+    exact hr1.trans' h.le
+  · lift r to (ValueGroupWithZero R)ˣ using IsUnit.mk0 r hr
+    use r ^ 2
+    simp only [Units.val_pow_eq_pow_val, setOf_subset_setOf, Prod.forall, true_and]
+    intro _ _ h
+    refine (h.trans ?_).le
+    exact pow_lt_self_of_lt_one₀ (by simp) hr1 (by simp)
+
 theorem isOpen_ball (r : ValueGroupWithZero R) :
     IsOpen {x | v x < r} := by
-  rw [isOpen_iff_mem_nhds]
-  rcases eq_or_ne r 0 with rfl | hr
+  rcases eq_or_ne r 0 with rfl|hr
   · simp
-  · intro x hx
-    rw [mem_nhds]
-    simp only [setOf_subset_setOf]
-    exact ⟨Units.mk0 _ hr,
-      fun y hy => (sub_add_cancel y x).symm ▸ ((v).map_add _ x).trans_lt (max_lt hy hx)⟩
+  convert ((v).isTransitiveRel_uniformity_ball_lt r).isOpen_ball_of_mem_uniformity 0
+    (uniformity_ball_lt_mem_uniformity hr)
+  simp [UniformSpace.ball]
 
 theorem isClosed_ball (r : ValueGroupWithZero R) :
     IsClosed {x | v x < r} := by
@@ -98,12 +145,9 @@ theorem isClopen_ball (r : ValueGroupWithZero R) :
 
 lemma isOpen_closedBall {r : ValueGroupWithZero R} (hr : r ≠ 0) :
     IsOpen {x | v x ≤ r} := by
-  rw [isOpen_iff_mem_nhds]
-  intro x hx
-  rw [mem_nhds]
-  simp only [setOf_subset_setOf]
-  exact ⟨Units.mk0 _ hr, fun y hy => (sub_add_cancel y x).symm ▸
-    le_trans ((v).map_add _ _) (max_le (le_of_lt hy) hx)⟩
+  convert ((v).isTransitiveRel_uniformity_ball_le r).isOpen_ball_of_mem_uniformity 0
+    (uniformity_ball_le_mem_uniformity hr)
+  simp [UniformSpace.ball]
 
 theorem isClosed_closedBall (r : ValueGroupWithZero R) :
     IsClosed {x | v x ≤ r} := by
